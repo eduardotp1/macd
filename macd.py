46204f969c9485815e0345a14fd2f9681c92ced1
eduardotp1 <edu.tirta@gmail.com>
2019-11-06 23:53:38 -0300

    fix-removing first element of list

--- macd.py
+++ macd.py
@@ -8,59 +8,58 @@ class MACD(Strategy):
 
     def __init__(self):
         self.signal = 0
-        self.historic = []
+        self.hist = []
         self.sizec = 12*7
         self.sized = 26*7
-        self.convergence = 0
-        self.divergence = 0
-        self.macdHistoric = []
-        self.size = 9*7
-        self.macdExpMa = None
+        self.conv = 0
+        self.div = 0
+        self.macd_hist = []
+        self.size = 9*5
+        self.exp_mm = None
 
     def push(self, event):
         orders = []
         if event.type == Event.TRADE:
             price = event.price
-            self.historic.append(price)
+            self.hist.append(price)
 
-            if len(self.historic) == self.sized:
-                alpha = 2/(self.sizec + 1)
-                self.convergence = (1-alpha)*sum(self.historic[self.sized-self.sizec:])/self.sizec + alpha*self.convergence
-                alpha = 2/(self.sized + 1)
-                self.divergence = (1-alpha)*sum(self.historic)/self.sized + alpha*self.divergence
+            if len(self.hist) == self.sized:
+                a = 2/(self.sizec + 1)
+                self.conv = (1-a)*sum(self.hist[self.sized-self.sizec:])/self.sizec + a*self.conv
+                a = 2/(self.sized + 1)
+                self.div = (1-a)*sum(self.hist)/self.sized + a*self.div
+                macd = self.conv - self.div
+                # print(macd)
+                self.macd_hist.append(macd)
 
-                macd = self.convergence - self.divergence
-                self.macdHistoric.append(macd)
+                if len(self.macd_hist) == self.size:
+                    if self.exp_mm is None:
+                        self.exp_mm = sum(self.macd_hist)/self.size
+                    else:
+                        new_mm = self.exp_mm
+                        a = 2/(self.size + 1)
+                        self.exp_mm = (1 - a)*new_mm + a*self.macd_hist[-1]
+                    diff = macd - self.exp_mm
 
-                if len(self.macdHistoric) == self.size:
-                    
-                    self.exponentialMa()
-                    diff = macd - self.macdExpMa
 
                     if self.signal != 1 and diff > 0.001:
                         if self.signal == -1:
+                            # print(orders)
                             orders.append(Order(event.instrument, 1, 0))
                         orders.append(Order(event.instrument, 1, 0))
                         self.signal = 1
+
+                        
                     elif self.signal != -1 and diff < -0.001:
                         if self.signal == 1:
+                            # print(orders)
                             orders.append(Order(event.instrument, -1, 0))
                         orders.append(Order(event.instrument, -1, 0))
                         self.signal = -1
-
-                    del self.macdHistoric[0]
-                
-                del self.historic[0]
-        
+                    del self.macd_hist[0]
+                del self.hist[0]
         return orders
 
-    def exponentialMa(self):
-        if self.macdExpMa is None:
-            self.macdExpMa = sum(self.macdHistoric)/self.size
-        else:
-            lastMa = self.macdExpMa
-            alpha = 2/(self.size + 1)
-            self.macdExpMa = (1 - alpha)*lastMa + alpha*self.macdHistoric[-1]
 
 
 print(evaluateTick(MACD(), {'PETR4': '2018-03-07.csv'}))
